79c590fab4fab650e35a56184ee511632eb8b424
MarlonY123 <A01424875@tec.mx>
2023-11-29 15:46:36 -0600

    bfs with capacity

--- evidencia3.py
+++ evidencia3.py
@@ -3,8 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # %%
-def BFS_levels(graph, s,levels):
-    remover = []
+def BFS_levels(graph, s,levels,t):
     visited = [False] * (max(graph) + 1)
     queue = []
     queue.append(s)
@@ -16,26 +15,29 @@ def BFS_levels(graph, s,levels):
         #print(s, end=" ")
         #print('Origen: ',s,' Nodos conectados: ', graph[s])
         for i in graph[s]:
-            if visited[i] == False:
+            capacidad = graph[s][i]['weight']
+            if visited[i] == False and  capacidad > 0:
+                print("De ", s," a: ", i ," capacidad:",graph[s][i]['weight'])
                 queue.append(i)
                 visited[i] = True
                 levels[i] = levels[s] + 1
-            else:
-                if(levels[s] > levels[i]):
-                    remover.append([s,i])
-    #Modificar para arreglar que se remuevan todos los nodos innecesarios
-    for i in remover:
-        graph.remove_edge(i[0],i[1])
+    return visited[t] != False
 # %%
 G = nx.DiGraph()
-G.add_nodes_from([0,1,2,3,4,5,6,7,8,9,10])
-G.add_weighted_edges_from([(0,1,5), (0,2,10), (0,3,15), (1,0,-5), (2,0,-10),(3,0,-15),(1,4,10),(1,2,0),(4,1,-5),(2,1,15),(2,5,20),(5,2,-10),(3,5,),(5,3),(3,6),(6,3),(5,4),(4,5),(4,7),(7,4),(5,8),(8,5),(6,8),(8,6),(6,9),(9,6),(4,8),(8,4),(7,10),(10,7),(8,10),(10,8),(8,9),(9,8),(9,10),(10,9)])
+s,t = 0,10
+G.add_nodes_from([s,1,2,3,4,5,6,7,8,9,t])
+G.add_weighted_edges_from([(0,1,5), (0,2,10), (0,3,15), (1,0,0), (2,0,0),(3,0,0),
+                           (1,4,10),(1,2,0),(4,1,0),(2,1,15),(2,5,20),(5,2,0),(3,5,0),
+                           (5,3,5),(3,6,25),(6,3,0),(5,4,0),(4,5,25),(4,7,10),(7,4,0),
+                           (5,8,30),(8,5,0),(6,8,20),(8,6,0),(6,9,10),(9,6,0),(4,8,0),
+                           (8,4,15),(7,10,5),(10,7,0),(8,10,15),(10,8,0),(8,9,15),(9,8,0),
+                           (9,10,10),(10,9,0)])
+
 pos = nx.spring_layout(G)
 nx.draw(G, pos,labels=dict([(n,n) for n in range(11)]))  
-print(list(G.successors(0)))
 # %% 
-#levels = {}
-#BFS_levels(G,0,levels)
-#nx.draw(G, pos,labels=levels)  
-#print(levels)
+levels = {}
+print(BFS_levels(G,s,levels,t))
+nx.draw(G, pos,labels=levels)  
+print(levels)
     # %%
